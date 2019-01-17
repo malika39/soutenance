f8191c4803bd1544c15796aab6e30c54d0d3734a
Hachimia <hachimia.msa@gmail.com>
2019-01-17 10:02:44 +0100

    Modifications
    Blog Controller et modifs templates

---
+++ src/Migrations/Version20190116223939.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190116223939 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, titre VARCHAR(255) NOT NULL, slug VARCHAR(255) NOT NULL, contenu LONGTEXT NOT NULL, featured_image VARCHAR(200) NOT NULL, date_creation DATETIME NOT NULL, INDEX IDX_23A0E66A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E66A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+        $this->addSql('DROP TABLE order_ateliersweets');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE order_ateliersweets (id INT AUTO_INCREMENT NOT NULL, ateliersweet_id INT NOT NULL, order_id INT NOT NULL, INDEX IDX_A9A5E0988D9F6D38 (order_id), UNIQUE INDEX uniq_orderateliersweet (order_id, ateliersweet_id), INDEX IDX_A9A5E0985CF55047 (ateliersweet_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE order_ateliersweets ADD CONSTRAINT FK_A9A5E0985CF55047 FOREIGN KEY (ateliersweet_id) REFERENCES ateliersweets (id)');
+        $this->addSql('ALTER TABLE order_ateliersweets ADD CONSTRAINT FK_A9A5E0988D9F6D38 FOREIGN KEY (order_id) REFERENCES orders (id)');
+        $this->addSql('DROP TABLE article');
+    }
+}
